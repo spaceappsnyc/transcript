5ba923aa95b5e0d1f3727035dcf986f5be8f8339
Benjamin Bojko <ben@potiondesign.com>
2015-04-12 01:27:51 -0400

    Wrapping text meshes from json

---
+++ tests/UnityTextTest/Assets/Scripts/MainController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+
+public class MainController : MonoBehaviour {
+
+	public GameObject largeTextPrefab;
+	public Vector3 canvasTopLeft;
+	public Vector3 canvasBottomRight;
+
+	JSONLoader<SegmentContainer> loader;
+
+	void Start ()
+	{
+		loader = new JSONLoader<SegmentContainer> ();
+		StartCoroutine (loader.Load ("http://localhost:8000/sample-segments.json", LoadSuccess, LoadError));
+	}
+
+	void LoadSuccess (SegmentContainer container)
+	{
+		Debug.Log ("Loaded " + container.Segments.Count + " segments");
+
+		float maxWidth = 0.5f * (canvasBottomRight.x - canvasTopLeft.x);
+		Vector3 currentPos = new Vector3 (canvasTopLeft.x, canvasTopLeft.y, 0.0f);
+		
+		foreach (Segment segment in container.Segments) {
+			GameObject gameObject = Instantiate(largeTextPrefab);
+			gameObject.transform.position = new Vector3(currentPos.x, currentPos.y, currentPos.z);
+
+			TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+			textMesh.text = segment.Text;
+			TextUtils.WrapTextMesh(textMesh, maxWidth);
+
+			currentPos.y -= textMesh.GetComponent<Renderer>().bounds.extents.y;
+		}
+	}
+
+	void LoadError (string error)
+	{
+		Debug.Log ("Error loading files: " + error);
+	}
+
+}
---
+++ tests/UnityTextTest/Assets/Scripts/TextUtils.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextUtils {
+	
+	public static void WrapTextMesh (TextMesh textMesh, float maxWidth)
+	{
+		string builder = "";
+		string text = textMesh.text;
+		string[] parts = text.Split(' ');
+
+		textMesh.text = "";
+
+		for (int i = 0; i < parts.Length; i++) {
+			textMesh.text += parts[i] + " ";
+			
+			if (textMesh.GetComponent<Renderer>().bounds.extents.x >= maxWidth) {
+				textMesh.text = builder.TrimEnd() + System.Environment.NewLine + parts[i] + " ";
+			}
+			
+			builder = textMesh.text;
+		}
+	}
+	
+}
